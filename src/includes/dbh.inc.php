b0524c6cd174c24c8166fb1e127dbea68e440ec0
dashIink <71524804+dashIink@users.noreply.github.com>
2021-11-24 19:48:21 -0500

    Attempting to get login working

---
+++ src/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPass = "";
+$dBName = "hotels";
+$db = mysqli_connect($serverName,$dBUsername,$dBPass,$dBName)or die("connection to database failed");
+?>
